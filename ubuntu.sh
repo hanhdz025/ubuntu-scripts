13ced0822021fe8c2a964cccbd2f5ba1a47d372a
Hanh Nguyen <hanh.cho.do@gmail.com>
2022-06-03 17:48:19 +0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -8,7 +8,7 @@ sudo apt install -y curl git terminator flameshot
 # install docker
 if ! [[ $(which docker) && $(docker --version) ]]; then
   sudo apt update
-  sudo apt install apt-transport-https ca-certificates curl software-properties-common
+  sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
   apt-cache policy docker-ce
@@ -27,14 +27,14 @@ npm install --global yarn && yarn -v
 if ! installed github-desktop; then
   wget -qO - https://mirror.mwt.me/ghd/gpgkey | sudo tee /etc/apt/trusted.gpg.d/shiftkey-desktop.asc > /dev/null
   sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/shiftkey/desktop/any/ any main" > /etc/apt/sources.list.d/packagecloud-shiftkey-desktop.list'
-  sudo apt update && sudo apt install github-desktop
+  sudo apt update && sudo apt install -y github-desktop
 fi
 
 # install ibus-bamboo
 if ! installed ibus-bamboo; then
   sudo add-apt-repository ppa:bamboo-engine/ibus-bamboo
   sudo apt-get update
-  sudo apt-get install ibus ibus-bamboo --install-recommends
+  sudo apt-get install -y ibus ibus-bamboo --install-recommends
   ibus restart
   # Đặt ibus-bamboo làm bộ gõ mặc định
   env DCONF_PROFILE=ibus dconf write /desktop/ibus/general/preload-engines "['BambooUs', 'Bamboo']" && gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('ibus', 'Bamboo')]"
@@ -45,7 +45,7 @@ docker run -d --name mariadb -p 3306:3306 -e MARIADB_ROOT_PASSWORD=root mariadb
 docker run -d --name redis -p 6379:6379 redis
 
 # install zsh
-sudo apt install zsh
+sudo apt install -y zsh
 # ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # zsh plugins
