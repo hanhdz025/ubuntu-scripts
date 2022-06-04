1dfeb5233c9a9eadcf757f33a53321ee3a1510ca
Hanh Nguyen <hanh.cho.do@gmail.com>
2022-06-04 21:59:37 +0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -12,7 +12,7 @@ if ! [[ $(which docker) && $(docker --version) ]]; then
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
   apt-cache policy docker-ce
-  sudo apt install docker-ce
+  sudo apt install -y docker-ce
   #sudo systemctl status docker
   sudo usermod -aG docker ${USER}
 fi
