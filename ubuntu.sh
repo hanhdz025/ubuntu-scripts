acbb36f6e6f9634e83a25da3237170e87c4c76a6
Hanh Nguyen <hanh.cho.do@gmail.com>
2022-12-24 21:08:41 +0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -17,9 +17,11 @@ if ! [[ $(which docker) && $(docker --version) ]]; then
   sudo usermod -aG docker ${USER}
 fi
 
-# install zsh
+# install zsh, ohmyzsh
 if ! installed zsh; then
   sudo apt install -y zsh
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+  chsh -s $(which zsh)
 fi
 
 # install nvm
@@ -50,9 +52,6 @@ fi
 docker run -d --name mariadb -p 3306:3306 -e MARIADB_ROOT_PASSWORD=root mariadb
 docker run -d --name redis -p 6379:6379 redis
 
-
-# ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 # zsh plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
