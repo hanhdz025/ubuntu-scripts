217485bea877bc0a9677fe276bffff65eaa239b6
Hanh Nguyen <hanh.cho.do@gmail.com>
2022-12-28 21:13:50 +0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,11 +2,16 @@ installed() {
   return $(dpkg-query -W -f '${Status}\n' "${1}" 2>&1|awk '/ok installed/{print 0;exit}{print 1}')
 }
 
+echo() {
+  command printf %s\\n "$*" 2>/dev/null
+}
+
 # install tools
 sudo apt install -y curl git terminator flameshot
 
 # install docker
 if ! [[ $(which docker) && $(docker --version) ]]; then
+  echo "install docker"
   sudo apt update
   sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -19,7 +24,9 @@ fi
 
 # install zsh, ohmyzsh
 if ! installed zsh; then
+  echo "install zsh"
   sudo apt install -y zsh
+  echo "install ohmyzsh"
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
   sudo chsh -s $(which zsh)
   exec zsh
@@ -27,14 +34,17 @@ fi
 
 # install nvm
 if [ -n "$NVM_DIR" ]; then
+  echo "install nvm"
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
   source ~/.zshrc
   nvm install --lts && node -v
   npm install --global yarn && yarn -v
+  npm install --global pnpm && pnpm -v
 fi
 
 # install git-desktop
 if ! installed github-desktop; then
+  echo "install github-desktop"
   wget -qO - https://mirror.mwt.me/ghd/gpgkey | sudo tee /etc/apt/trusted.gpg.d/shiftkey-desktop.asc > /dev/null
   #sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/shiftkey/desktop/any/ any main" > /etc/apt/sources.list.d/packagecloud-shiftkey-desktop.list'
   sudo sh -c 'echo "deb [arch=amd64] https://mirror.mwt.me/ghd/deb/ any main" > /etc/apt/sources.list.d/packagecloud-shiftkey-desktop.list'
@@ -43,6 +53,7 @@ fi
 
 # install ibus-bamboo
 if ! installed ibus-bamboo; then
+  echo "install ibus-bamboo"
   sudo add-apt-repository ppa:bamboo-engine/ibus-bamboo
   sudo apt-get update
   sudo apt-get install -y ibus ibus-bamboo --install-recommends
@@ -57,9 +68,10 @@ fi
 
 # zsh plugins
 if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-nvm ]; then
-  git clone https://github.com/lukechilds/zsh-nvm ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-nvm
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  echo "install zsh plugins"
+  #git clone https://github.com/lukechilds/zsh-nvm ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-nvm
+  #git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  #git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
   # add zsh-nvm zsh-autosuggestions zsh-syntax-highlighting
-  echo "plugins+=(zsh-nvm zsh-autosuggestions zsh-syntax-highlighting)" >> ~/.zshrc
+  #echo "plugins+=(zsh-nvm zsh-autosuggestions zsh-syntax-highlighting)" >> ~/.zshrc
 fi
